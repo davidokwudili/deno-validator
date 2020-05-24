ab3e77f8a72e1e76e4f3f2f39a206e536710c65a
Paras Gupta <parasg1999@gmail.com>
2020-05-24 22:17:51 +0530

    Added full and half width

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
 export { isEmpty } from "./lib/isEmpty.ts";
-export { isUpperCase } from "./lib/isUpperCase.ts";
+export { isFullWidth } from "./lib/isFullWidth.ts";
+export { isHalfWidth } from "./lib/isHalfWidth.ts";
 export { isLowerCase } from "./lib/isLowerCase.ts";
+export { isUpperCase } from "./lib/isUpperCase.ts";
--- test/test.ts
+++ test/test.ts
@@ -4,21 +4,60 @@ import {
 } from "https://deno.land/std/testing/asserts.ts";
 import * as validator from "../mod.ts";
 
-Deno.test('isEmpty', function(): void {
-    assertEquals(validator.isEmpty(""),true);
-    assertEquals(validator.isEmpty("any string"),false);
-    assertEquals(validator.isEmpty("   "),false);
-    assertEquals(validator.isEmpty("   ",{ignore_whitespace:true}),true);
+Deno.test('isEmpty', function (): void {
+    assertEquals(validator.isEmpty(""), true);
+    assertEquals(validator.isEmpty("foo"), false);
+    assertEquals(validator.isEmpty("   "), false);
+    assertEquals(validator.isEmpty("   ", { ignore_whitespace: true }), true);
 })
 
-Deno.test('isUpperCase', function(): void {
-    assertEquals(validator.isUpperCase(""),true);
-    assertEquals(validator.isUpperCase("any string"),false);
-    assertEquals(validator.isUpperCase(" ANYTHING  "),true);
+Deno.test('isFullWidth', function (): void {
+    [
+        'ひらがな・カタカナ、．漢字',
+        '３ー０　ａ＠ｃｏｍ',
+        'Ｆｶﾀｶﾅﾞﾬ',
+        'Good＝Parts',
+    ].forEach(e => {
+        assertEquals(validator.isFullWidth(e), true)
+    });
+
+    [
+        'abc',
+        'abc123',
+        '!"#$%&()<>/+=-_? ~^|.,@`{}[]',
+    ].forEach(e => {
+        assertEquals(validator.isFullWidth(e), false)
+    });
 })
 
-Deno.test('isLowerCase', function(): void {
-    assertEquals(validator.isLowerCase(""),true);
-    assertEquals(validator.isLowerCase("any string"),true);
-    assertEquals(validator.isLowerCase(" ANYTHING  "),false);
+Deno.test('isHalfWidth', function (): void {
+    [
+        '"#$%&()<>/+=-_? ~^|.,@`{}[]',
+        'l-btn_02--active',
+        'abc123い',
+        'ｶﾀｶﾅﾞﾬ￩',
+    ].forEach(e => {
+        assertEquals(validator.isHalfWidth(e), true)
+    });
+
+    [
+        'あいうえお',
+        '００１１',
+    ].forEach(e => {
+        assertEquals(validator.isHalfWidth(e), false)
+    });
+})
+
+Deno.test('isLowerCase', function (): void {
+    assertEquals(validator.isLowerCase(""), true);
+    assertEquals(validator.isLowerCase("foo"), true);
+    assertEquals(validator.isLowerCase("BAR"), false);
+    assertEquals(validator.isLowerCase("BaZ"), false);
+})
+
+Deno.test('isUpperCase', function (): void {
+    assertEquals(validator.isUpperCase(""), true);
+    assertEquals(validator.isUpperCase("foo"), false);
+    assertEquals(validator.isUpperCase("BAR"), true);
+    assertEquals(validator.isUpperCase("BaZ"), false);
 })
